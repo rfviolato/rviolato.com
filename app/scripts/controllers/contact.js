029a02e3b05687fe367b4bce6b2cac27079e5a2f
Rafael Violato <rfviolato@gmail.com>
2015-09-16 10:40:51 -0300

    Rewrite contact controller

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -8,34 +8,22 @@
  * Controller of the rviolatocomApp
  */
 angular.module('rviolatocomApp')
-  .controller('ContactCtrl', ['$scope', '$http', function ($scope, $http) {
-  	  var self = this;
+  .controller('ContactCtrl', ContactCtrl);
+
+  ContactCtrl.$inject = ['$scope', '$http'];
+
+  function ContactCtrl($scope, $http) {
+      var vm = this;
       $scope.mainController.header.menuOpened = false;
-      self.sendingEmail = false;
-      self.sendEmail = function(){
-         if(!self.sendingEmail){            
-            var _name = self.name;
-            var _email = self.email;
-            var _message = self.message;
-            var _emailSent = function(){
-                self.sendingEmail = false;
-                self.name = '';
-                self.email = '';
-                self.message = '';
-                if($scope.mainController.language === 'pt'){
-					$scope.mainController.texts.contact.formDescriptionText = 'Sua mensagem foi enviada.';
-                }else{
-                	$scope.mainController.texts.contact.formDescriptionText = 'Message sent.';
-                }
-            };
-            var _emailError = function(){
-                self.sendingEmail = false;
-                if($scope.mainController.language === 'pt'){
-					$scope.mainController.texts.contact.formDescriptionText = 'Oops, algo errado aconteceu :/';
-                }else{
-                	$scope.mainController.texts.contact.formDescriptionText = 'Oops, something wrong happened :/';
-                }
-            };
+      vm.sendingEmail = false;
+      vm.sendEmail = sendEmail;
+
+      function sendEmail(){
+         if(!vm.sendingEmail){            
+            var _name = vm.name;
+            var _email = vm.email;
+            var _message = vm.message;
+
             if(_name){
                if(_email){
                   if(_message){
@@ -45,40 +33,63 @@ angular.module('rviolatocomApp')
                         email: _email,
                         message: _message,
                      };
-                     console.log(_data);
+                     
                      var _promise = $http({
                          method: 'post',
                          url: 'enviaEmail.php',
                          data: _data,
                          headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
                      });                     
-                     //var _promise = $http.post('enviaEmail.php', _data);
-                     self.sendingEmail = true;
 
-                     _promise.success(_emailSent);
-                     _promise.error(_emailError);
-                     // ---- ---- ----
+                     vm.sendingEmail = true;
+
+                     _promise.success(emailSent);
+                     _promise.error(emailError);
+                     
                   }else{
-                  	if($scope.mainController.language === 'pt'){
-						$scope.mainController.texts.contact.formDescriptionText = 'Insira uma mensagem.';
-                  	}else{
-                  		$scope.mainController.texts.contact.formDescriptionText = 'Insert a message.';
-                  	}
+                    if($scope.mainController.language === 'pt'){
+                      $scope.mainController.texts.contact.formDescriptionText = 'Insira uma mensagem.';
+                    }else{
+                      $scope.mainController.texts.contact.formDescriptionText = 'Insert a message.';
+                    }
                   }
                }else{
-               	if($scope.mainController.language === 'pt'){
-					$scope.mainController.texts.contact.formDescriptionText = 'Insirir email.';
-               	}else{
-               		$scope.mainController.texts.contact.formDescriptionText = 'Insert email.';
-               	}
+                if($scope.mainController.language === 'pt'){
+                  $scope.mainController.texts.contact.formDescriptionText = 'Insirir email.';
+                }else{
+                  $scope.mainController.texts.contact.formDescriptionText = 'Insert email.';
+                }
                }
             }else{
-            	if($scope.mainController.language === 'pt'){
-					$scope.mainController.texts.contact.formDescriptionText = 'Insirir nome.';
-            	}else{
-            		$scope.mainController.texts.contact.formDescriptionText = 'Insert name.';
-            	}
+              if($scope.mainController.language === 'pt'){
+                $scope.mainController.texts.contact.formDescriptionText = 'Insirir nome.';
+              }else{
+                $scope.mainController.texts.contact.formDescriptionText = 'Insert name.';
+              }
             }            
          }
-      };
-    }]);
+      }
+
+      function emailSent(){
+          vm.sendingEmail = false;
+          vm.name = '';
+          vm.email = '';
+          vm.message = '';
+
+          if($scope.mainController.language === 'pt'){
+            $scope.mainController.texts.contact.formDescriptionText = 'Sua mensagem foi enviada.';
+          }else{
+            $scope.mainController.texts.contact.formDescriptionText = 'Message sent.';
+          }
+      }
+
+      function emailError(){
+          vm.sendingEmail = false;
+
+          if($scope.mainController.language === 'pt'){
+            $scope.mainController.texts.contact.formDescriptionText = 'Oops, algo errado aconteceu :/';
+          }else{
+            $scope.mainController.texts.contact.formDescriptionText = 'Oops, something wrong happened :/';
+          }
+      }
+    }
