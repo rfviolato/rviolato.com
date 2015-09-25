9d14dc6623fb73cafb89f5906eb9c3ce1bbc5b6e
Rafael Violato <rfviolato@gmail.com>
2015-09-25 17:31:20 -0300

    Rewrite background directive

--- app/scripts/directives/background.js
+++ app/scripts/directives/background.js
@@ -7,37 +7,46 @@
  * # background
  */
 angular.module('rviolatocomApp')
-  .directive('background', function ($rootScope) {
-    return {
+  .directive('background', background);
+
+  background.$inject = ['$rootScope'];
+
+  function background($rootScope) {
+    var directive = {
       restrict: 'C',
       replace: true,
-      link: function postLink(scope, element) {
-        var url = window.getComputedStyle(element[0]).backgroundImage
-          .replace('url(', '')
-          .replace(')', '')
-          .replace(/"/g, '');
-        var dummyImg = document.createElement('img');
-        var load = function(){
-            element.css('background-image', 'url('+url+')');
-            element.addClass('loaded');
-            $rootScope.$broadcast('background-load', {
-              status: 'loaded',
-            });
-        };
-        var error = function(){
-        	console.error('background image loading error :/');
-        	$rootScope.$broadcast('background-load', {
-        		status: 'error',
-        	});        	
-        };
-
-        dummyImg.addEventListener('load', load);
-        dummyImg.addEventListener('error', error);
-        dummyImg.src = url;
-
-      	if (dummyImg.complete) {
-  			 load();
-      	}
-      }
+      link: postLink
     };
-  });
+
+    return directive;
+
+    function postLink(scope, element) {
+      var url = window.getComputedStyle(element[0]).backgroundImage
+        .replace('url(', '')
+        .replace(')', '')
+        .replace(/"/g, '');
+      var dummyImg = document.createElement('img');
+      dummyImg.addEventListener('load', load);
+      dummyImg.addEventListener('error', error);
+      dummyImg.src = url;
+
+      if (dummyImg.complete) {
+       load();
+      }
+
+      function load(){
+          element.css('background-image', 'url('+url+')');
+          element.addClass('loaded');
+          $rootScope.$broadcast('background-load', {
+            status: 'loaded',
+          });
+      }
+
+      function error(){
+        console.error('background image loading error :/');
+        $rootScope.$broadcast('background-load', {
+          status: 'error',
+        });
+      }
+    }
+  }
