bbaf0d838780a3180c9eaa3536a2838bbece3752
Rafael Violato <rfviolato@gmail.com>
2015-10-22 10:31:16 -0200

    Refactor app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,37 +11,39 @@
 angular
   .module('rviolatocomApp', [
     'ngAnimate',
-    'ngCookies',
     'ngResource',
     'ngRoute',
-    'ngSanitize',
-    'ngTouch'
+    'ngSanitize'
   ])
-  .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-      $locationProvider.html5Mode(false);
-      $routeProvider
-        .when('/', {
-          templateUrl: 'views/hello.html',
-          controller: 'HelloCtrl',
-          controllerAs: 'hello',
-        })
-        .when('/projects', {
-          templateUrl: 'views/projects.html',
-          controller: 'ProjectsCtrl',
-          controllerAs: 'projects',
-        })
-        .when('/contact', {
-          templateUrl: 'views/contact.html',
-          controller: 'ContactCtrl',
-          controllerAs: 'contact',
-        })
-        .when('/repositories', {
-          templateUrl: 'views/repositories.html',
-          controller: 'RepositoriesCtrl',
-          controllerAs: 'repos',
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
+  .config(config);
 
-    }]);
+  config.$inject = ['$routeProvider', '$locationProvider'];
+
+  function config($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(false);
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/hello.html',
+        controller: 'HelloCtrl',
+        controllerAs: 'hello',
+      })
+      .when('/projects', {
+        templateUrl: 'views/projects.html',
+        controller: 'ProjectsCtrl',
+        controllerAs: 'projects',
+      })
+      .when('/contact', {
+        templateUrl: 'views/contact.html',
+        controller: 'ContactCtrl',
+        controllerAs: 'contact',
+      })
+      .when('/repositories', {
+        templateUrl: 'views/repositories.html',
+        controller: 'RepositoriesCtrl',
+        controllerAs: 'repos',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+  }
